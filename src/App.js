8dc4f4dab6d74ee91589250795ea91c6d3092b71
maurojaf <maurojaf@gmail.com>
2020-07-26 21:05:23 -0400

    Maqueta Venta (DONE)

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
     createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Cupcako', 305, 3.7, 67, 4.3),
     createData('Gingerbread', 356, 16.0, 49, 3.9),
   ];
   function createData(name, calories, fat, carbs, protein) {
